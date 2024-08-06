d82a556be8632ff131cc5d859d904ed8723a7e88
Paul Ebose <paulebose@gmail.com>
2024-08-06 14:58:08 +0100

    :control_knobs: set text and bg colors for the document root and theme-color meta tag
    - Also remove the custom styles for `.fluentui-root`

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ const rootElement = document.getElementById('root')
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <FluentProvider theme={webDarkTheme} className="fluentui-root">
+      <FluentProvider theme={webDarkTheme}>
         <RouterProvider router={appRouter} />
       </FluentProvider>
     </StrictMode>,
