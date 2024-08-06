b79627343b04d1902137fc114d8049461c19eb26
Paul Ebose <paulebose@gmail.com>
2024-08-06 19:13:39 +0100

    :godmode: set up storybook previews with fluent-ui

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,1 +1,13 @@
 import '../src/index.css'
+import { FluentProvider, webDarkTheme } from '@fluentui/react-components'
+import { withActions } from '@storybook/addon-actions/decorator'
+import type { Decorator } from '@storybook/react'
+
+export const decorators: Decorator[] = [
+  withActions,
+  (Story) => (
+    <FluentProvider theme={webDarkTheme}>
+      <Story />
+    </FluentProvider>
+  ),
+]
