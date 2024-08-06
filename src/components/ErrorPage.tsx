9dae6b81fe0203653bf87baeb69fb48c000088ba
Paul Ebose <paulebose@gmail.com>
2024-08-06 15:07:01 +0100

    :godmode: update error page with fluent-ui components

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,51 +1,82 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
+import { LargeTitle, makeStyles, mergeClasses, Subtitle1, Text, tokens } from '@fluentui/react-components'
 import { isRouteErrorResponse, useRouteError } from 'react-router-dom'
+import Link from '~/components/Link'
+
+const useStyles = makeStyles({
+  text: {
+    display: 'block',
+    marginTop: tokens.spacingVerticalM,
+    textAlign: 'center',
+  },
+  subtitle: {
+    marginTop: tokens.spacingVerticalXXXL,
+  },
+  container: {
+    width: 'fit-content',
+    margin: 'auto',
+    marginTop: tokens.spacingVerticalXXXL,
+  },
+})
 
 function ErrorPage() {
+  const styles = useStyles()
   const error = useRouteError()
   if (import.meta.env.DEV) console.warn(error)
 
   if (isRouteErrorResponse(error)) {
     return (
-      <div id="error-page">
-        <h1>Oops!</h1>
-        <h2>{error.status}</h2>
-        <p>{error.statusText}</p>
-        {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-          error.data.message ? (
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-            <p>{error.data.message}</p>
-          ) : (
-            <code>{JSON.stringify(error.data)}</code>
-          )
-        }
-        <p>
-          <a href="/">Go Home</a>
-        </p>
+      <div id="error-page" className={styles.container}>
+        <LargeTitle as="h1">
+          {error.status}: {error.statusText}
+        </LargeTitle>
+        {error.data.message ? (
+          <Text as="p" className={styles.text}>
+            {error.data.message}
+          </Text>
+        ) : (
+          <Text as="pre" className={styles.text}>
+            {JSON.stringify(error.data)}
+          </Text>
+        )}
+        <Subtitle1 as="p" className={mergeClasses(styles.text, styles.subtitle)}>
+          <Link href="/">Go Home</Link>
+        </Subtitle1>
       </div>
     )
   }
 
   if (error instanceof Error) {
     return (
-      <div id="error-page">
-        <h1>Oops! Unexpected Error</h1>
-        <p>Something went wrong.</p>
-        <code>{JSON.stringify(error.message)}</code>
-        <p>
-          <a href="/">Go Home</a>
-        </p>
+      <div id="error-page" className={styles.container}>
+        <Text as="h1" className={styles.text}>
+          Oops! Unexpected Error
+        </Text>
+        <Text as="p" className={styles.text}>
+          Something went wrong.
+        </Text>
+        <Text as="pre" className={styles.text}>
+          {error.message}
+        </Text>
+        <Subtitle1 as="p" className={mergeClasses(styles.text, styles.subtitle)}>
+          <Link href="/">Go Home</Link>
+        </Subtitle1>
       </div>
     )
   }
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unknown error has occurred.</p>
-      <p>
-        <a href="/">Go Home</a>
-      </p>
+    <div id="error-page" className={styles.container}>
+      <Text as="h1" className={styles.text}>
+        Oops!
+      </Text>
+      <Text as="p" className={styles.text}>
+        Sorry, an unknown error has occurred.
+      </Text>
+      <Subtitle1 as="p" className={mergeClasses(styles.text, styles.subtitle)}>
+        <Link href="/">Go Home</Link>
+      </Subtitle1>
     </div>
   )
 }
