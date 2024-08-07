13cc734393658b68480570328cf09ffbe1cb6228
Paul Ebose <paulebose@gmail.com>
2024-08-07 17:43:45 +0100

    :control_knobs: remove unnecessary assets

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,13 @@
 import { useState } from 'react'
 import { Button } from '@fluentui/react-components'
-import viteLogo from '/vite.svg'
 import Link from '~/components/Link'
-import reactLogo from '../assets/react.svg'
 
 function IndexRoute() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Devtuls</h1>
       <div className="card">
         <p>
           <Link href="/about">About Devtuls</Link>
