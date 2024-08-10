2ad5c40bbd7e5f4989a8e1b9fe5f8f371c24dbd0
Paul Ebose <paulebose@gmail.com>
2024-08-10 15:49:24 +0100

    :godmode: implement form submission in DnsLookupRouteTool

--- src/routes/dns-lookup/ui/dns-lookup-route-tool.tsx
+++ src/routes/dns-lookup/ui/dns-lookup-route-tool.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, type FormEvent } from 'react'
 import { Button, Field, Input, makeStyles, Spinner, tokens } from '@fluentui/react-components'
 import { GlobeSearchFilled } from '@fluentui/react-icons'
-import { Form, useLoaderData, useNavigation } from 'react-router-dom'
+import { Form, useLoaderData, useNavigation, useSubmit } from 'react-router-dom'
 import type { DnsLookupLoaderData } from '../page.router'
 
 const useStyles = makeStyles({
@@ -25,6 +25,7 @@ const useStyles = makeStyles({
 
 function DnsLookupRouteTool() {
   const styles = useStyles()
+  const submit = useSubmit()
   const loaderData = useLoaderData() as DnsLookupLoaderData
   const navigation = useNavigation()
   const submitting = navigation.state === 'submitting'
@@ -35,9 +36,26 @@ function DnsLookupRouteTool() {
     setDomainName(loaderData.domainName)
   }, [loaderData.domainName])
 
+  const handleSubmit = (ev: FormEvent<HTMLFormElement>) => {
+    ev.preventDefault()
+    // We are staying on the same page after the form's action is done executing.
+    // Do not disable any form element so focus can remain on the submit button.
+    // Just ignore if the user continues to click while submitting.
+    if (submitting) return
+    // Extract relevant fields from the loader data.
+    const submitData = { ...loaderData }
+    delete submitData.openToolConfig
+    // Submit form data to the route action. Everything is executed in the browser.
+    submit(submitData, {
+      encType: 'application/json',
+      method: 'POST',
+      preventScrollReset: true,
+    })
+  }
+
   return (
     <div className={styles.container}>
-      <Form id="dnsLookup" method="POST">
+      <Form id="dnsLookup" method="POST" onSubmit={handleSubmit}>
         <Field label="Domain Name" size="large" className={styles.domainNameField}>
           <Input
             form="dnsLookup"
