ee5cb359eb7d86cb265d6fcdf4b8b292c1f99388
Paul Ebose <paulebose@gmail.com>
2024-08-13 00:16:19 +0100

    :control_knobs: improve the default appearance of devtuls homepage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,36 +1,57 @@
-import { useState } from 'react'
-import { Button } from '@fluentui/react-components'
+import { Caption1, Card, CardHeader, makeStyles, Text, tokens } from '@fluentui/react-components'
 import Link from '~/components/Link'
 
+const useStyles = makeStyles({
+  container: {
+    padding: tokens.spacingVerticalM,
+    textAlign: 'center',
+  },
+  heading: {
+    fontSize: tokens.fontSizeHero900,
+    marginTop: tokens.spacingVerticalXXXL,
+    fontFamily: tokens.fontFamilyMonospace,
+  },
+  subtitle: {
+    fontSize: tokens.fontSizeBase500,
+    marginTop: tokens.spacingVerticalXXXL,
+    fontFamily: tokens.fontFamilyMonospace,
+  },
+  subtitle2: {
+    fontSize: tokens.fontSizeBase300,
+    marginTop: tokens.spacingVerticalXXXL,
+    color: tokens.colorNeutralForeground2,
+  },
+  card: {
+    marginTop: tokens.spacingVerticalXXXL,
+  },
+  caption: {
+    color: tokens.colorNeutralForeground3,
+  },
+})
+
 function IndexRoute() {
-  const [count, setCount] = useState(0)
+  const styles = useStyles()
 
   return (
-    <>
-      <h1>Devtuls</h1>
-      <div className="card">
-        <p>
-          <Link href="/about">About Devtuls</Link>
-        </p>
-        <p>
-          <Link href="/dns-lookup">DNS Lookup</Link>
-        </p>
-        <p>
-          <Link href="/err">Error page</Link>
-        </p>
-        <Button
-          onClick={() => {
-            setCount((count) => count + 1)
-          }}
-        >
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Devtuls</h1>
+
+      <p className={styles.subtitle}>Collection of awesome developer tools available as a progressive web application.</p>
+
+      <p className={styles.subtitle2}>This application is focused on usage in mobile devices.</p>
+
+      <Card className={styles.card}>
+        <CardHeader
+          header={<Text weight="semibold">DNS Lookup</Text>}
+          description={
+            <Caption1 className={styles.caption}>
+              Query any DNS resolver service that supports DNS-over-HTTPS and JSON queries.
+            </Caption1>
+          }
+          action={<Link href="/dns-lookup">open</Link>}
+        />
+      </Card>
+    </div>
   )
 }
 
